bc5ae2995069a1241243395c60af998cd11a00a8
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-11-10 07:31:25 +0000

    style: expense tab updated

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -6,15 +6,17 @@ function Expense(props) {
    
 
   return (
-    <div className='expenses_tab'>
-      <section className='date_name'>
-        <ExpenseDate date = {props.date} />
-        <h3>{props.title}</h3>
-      </section>
-      <section className='price'>
-        <h3> ${props.price}</h3>
-      </section>
-    </div>
+     
+      <div className='expenses_tab'>
+        <section className='date_name'>
+          <ExpenseDate date={props.date} />
+          <h3>{props.title}</h3>
+        </section>
+        <section className='price'>
+          <h3> ${props.price}</h3>
+        </section>
+      </div>
+
   );
 }
 
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,32 +1,35 @@
 import React from 'react'
 import Expense from './Expense';
+import './ExpenseItem.css'
 
 
 function ExpenseItem(props) {
   return (
-    <React.Fragment>
+    <div>
       <h2>Expense Tracker</h2>
-      <Expense
-        title={props.items[0].title}
-        price={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <Expense
-        title={props.items[1].title}
-        price={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <Expense
-        title={props.items[2].title}
-        price={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <Expense
-        title={props.items[3].title}
-        price={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </React.Fragment>
+      <div className='expense_container'>
+        <Expense
+          title={props.items[0].title}
+          price={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <Expense
+          title={props.items[1].title}
+          price={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <Expense
+          title={props.items[2].title}
+          price={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <Expense
+          title={props.items[3].title}
+          price={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </div>
+    </div>
   );
 }
 
